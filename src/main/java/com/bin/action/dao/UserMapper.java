84c2587f723d7297f1e9df8fdc1e8a8f450edf18
zhangbin <852774731@qq.com>
2017-12-14 20:07:57 +0800

    使用generator初始化

---
+++ src/main/java/com/bin/action/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.bin.action.dao;
+
+import com.bin.action.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
